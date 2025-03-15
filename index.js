7cc1e6d586a21748ca22906b39f7e2c98069a797
Claire Cardie <ckcardie@gmail.com>
2025-03-15 14:29:55 -0700

    wrote display and hide functions for task input class

---
+++ index.js
@@ -0,0 +1,9 @@
+//togging display/hide with task input class
+function displayTaskInput() {
+    document.getElementById("task_input").style.display = "none";
+}
+function hideTaskInput() {
+    document.getElementById("task_input").style.display = "flex";
+}
+
+
