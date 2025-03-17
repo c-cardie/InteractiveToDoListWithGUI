e1cf3a340c4d684a92c60971892220eb0f067f2d
c-cardie <ccardie001@student.franciscan.edu>
2025-03-17 10:06:53 -0400

    made some changes on tablet

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+/*
 //togging display/hide with task input class
+//event listener
+document.getElementById("Add Task").addEventListener("click", displayTaskInput);
+//functions
 function displayTaskInput() {
-    document.getElementById("task_input").style.display = "none";
-}
-function hideTaskInput() {
     document.getElementById("task_input").style.display = "flex";
 }
+function hideTaskInput() {
+    document.getElementById("task_input").style.display = "none";
+}
+*/
+
+
+
+
+
 
 
