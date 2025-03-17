d54c52e304e3dc9cfed3ba40cfeafc1b211d9f83
Claire Cardie <ckcardie@gmail.com>
2025-03-17 11:45:04 -0400

    added functionality for clicking add button to add user text as a list item

--- index.js
+++ index.js
@@ -11,9 +11,31 @@ function hideTaskInput() {
 }
 */
 
+//identify list
+//same logic as other two variables
+const list = document.getElementById('list');
 
+//create taskText - an input variable
+//taskText looks for element named 'new_task' - the input element
+const taskText = document.getElementById('new_task');
 
+//create 'Add' - a variable that looks for element named 'Add'
+//the 'Add' element is the add button
+const Add = document.getElementById('Add');
+Add.addEventListener('click', (event) => {
+    console.log(new_task.value);
 
+    //create a new li
+    const li = document.createElement('li');
+    //create a variable to store text for new task
+    const taskText = document.createTextNode(new_task.value);
+    //append text to li
+    li.appendChild(taskText);
+    //append new li to list
+    list.appendChild(li);
+    //make typed task disappear in input box after adding a task
+    new_task.value = '';
+} )
 
 
 
