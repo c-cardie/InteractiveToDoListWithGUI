575e41d0c7e7d2b57c7a2a0e7437f1e0af8eff68
Claire Cardie <ckcardie@gmail.com>
2025-03-30 18:26:31 -0400

    attempting to add the text of each task to an array to be put in local storage as a JSON string...struggling

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-/*
-//togging display/hide with task input class
-//event listener
-document.getElementById("Add Task").addEventListener("click", displayTaskInput);
-//functions
-function displayTaskInput() {
-    document.getElementById("task_input").style.display = "flex";
-}
-function hideTaskInput() {
-    document.getElementById("task_input").style.display = "none";
-}
-*/
-
 //identify list
 //same logic as other two variables
 const list = document.getElementById('list');
@@ -19,11 +6,13 @@ const list = document.getElementById('list');
 //taskText looks for element named 'new_task' - the input element
 const taskText = document.getElementById('new_task');
 
+var toStorage = [];
+
 //create 'Add' - a variable that looks for element named 'Add'
 //the 'Add' element is the add button
 const Add = document.getElementById('Add');
 Add.addEventListener('click', (event) => {
-    console.log(new_task.value);
+    //console.log(new_task.value);
 
     //create a new li
     const li = document.createElement('li');
@@ -45,6 +34,13 @@ Add.addEventListener('click', (event) => {
     li.appendChild(deleteBtn);
     //append new li to list
     list.appendChild(li);
+
+    toStorage.push(taskText);
+    //console.log(toStorage);
+    //console.log(toStorage[0]);
+
+   
+
     //make typed task disappear in input box after adding a task
     new_task.value = '';
 } )
@@ -70,20 +66,83 @@ Remove.addEventListener('click', (event) => {
 })
 
 
+function save() {
+
+    //1.) tried iterating over toStorage, but it says it is not an iterable
+    //tasks = Array.from(toStorage.children).map(each => each.innerHTML);
+    //console.log(tasks);
+    //tasksJSON = JSON.stringify(tasks);
+    //console.log(tasksJSON);
+    //tried making it an iterator, but that didn't work either
+    //const iterator = toStorage.entries();
+    //console.log(iterator.next().value, iterator.next().done);
+
+    /*
+    2.) tried stringifying each task individually, then pushing those onto an array
+    var tasksJSON = [];
+
+    for(var i = 0; i<toStorage.length; i++) {
+
+        //console.log(toStorage[i]);
+        stringifiedTask = JSON.stringify(toStorage[i]);
+        tasksJSON.push(stringifiedTask);
+        console.log(tasksJSON);
+
+    }
+    */
+
+    // 3.) stringifying the whole array and storing that in localStorage
+    // for 2.) and 3.) I keep getting output as {}, {} - not the actual strings
+    var str = JSON.stringify(toStorage);
+    localStorage.setItem("savedTasks", str);
+
+}
+
+function getTasks() {
+
+    var str = localStorage.getItem("savedTasks");
+    var fromStorage = JSON.parse(str);
+    console.log(fromStorage);
+
+}
+
+
+window.onload = function() {
+    getTasks();
+}
+
+
+
+
+
+
+
+
 /*
-//add event listeners to all delete buttons
-//get from LI instead of deleteBtnList!!
-var deleteBtnList = document.querySelectorAll(".delete");
-deleteBtnList.forEach(deleteBtn => {
-    deleteBtn.addEventListener("click", (event) => {
+function saveAll() {
+    //Create an array to store the li values
+    var toStorage = [];
+    var values = document.querySelectorAll('li');
+    //Cycle through the li array
+    for (var i = 0; i < values.length; i++) {
+      toStorage.push(values[i]);
+      console.log(toStorage[i]);
+    }
 
-        //access its parent (li)
-        //console.log(deleteBtn.parentElement.nodeName);
-        //delete the li element
-        this.style.visibility = "hidden";
+}
+*/
 
-    })
-})
+/*
+function loadAll(array) {
+    for (var i = 0; i < array.length; i++) {
+        list.appendChild(array[i]);
+    }
+    
+  }
+
+window.onload = function() {
+    loadAll();
+}
 */
 
 
